461ac582680c52adf5cdd26ed6a2ee0ed2f61b89
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-03-12 20:42:12 +0100

    DoplněníinnerHTML

--- catering.js
+++ catering.js
@@ -13,21 +13,31 @@ Vyzkoušjte si vaše funkce v konzoli. Poté napište funkci createEvent, která
 */
 
 const justFood = (people) => {
-    return `catering od Just Food pro ${people} lidí za 100000 Kč`
+    return `catering od Just Food pro ${people} lidí za ${Math.round(people * priceJustFood)} Kč`
 }
 
 const yourMama = (people) => {
-    return `catering od Your Mama pro ${people} lidí za 200000 Kč`
+    return `catering od Your Mama pro ${people} lidí za ${Math.round(people * priceYourMama)} Kč`
 }
 
 const flavourHaven = (people) => {
-    return `catering od Flavour Haven pro ${people} lidí za 300000 Kč`
+    return `catering od Flavour Haven pro ${people} lidí za ${Math.round(people * priceFlavourHaven)} Kč`
 }
 
 const createEvent = (nameEvent, people, catering) => {
     return `Událost ${nameEvent} s ${catering(people)}`
 }
 
-console.log(createEvent('Birthday party', 1000, justFood))
-console.log(createEvent('Branch opening', 2000, yourMama))
-console.log(createEvent('Ballet Gala', 3000, flavourHaven))
+let priceJustFood = 200
+let priceYourMama = 400
+let priceFlavourHaven = 1000
+
+console.log(createEvent('Birthday party', 100, justFood))
+console.log(createEvent('Branch opening', 200, yourMama))
+console.log(createEvent('Ballet Gala', 300, flavourHaven))
+
+document.body.innerHTML += `
+    <p>${createEvent('Birthday party', 100, justFood)}</p>
+    <p>${createEvent('Branch opening', 200, yourMama)}</p>
+    <p>${createEvent('Ballet Gala', 300, flavourHaven)}</p>
+`
